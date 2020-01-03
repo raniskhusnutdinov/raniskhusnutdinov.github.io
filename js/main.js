dbece7c89c92f019263935556cac0cdecf7fbc5f
Ранис Хуснутдинов <ronyquest74@gmail.com>
2020-01-03 11:38:21 +0500

    fix any bags

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close')
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible')
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
-});
-*/
 $(document).ready(function () {
   var modal = $('.modal'),
      modalBtn = $('[data-toggle=modal]'),
@@ -128,3 +115,6 @@ $(document).ready(function () {
   });
 
 });
+
+function newFunction() {
+}
